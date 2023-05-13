f5be7567d67796f84cc696cd37f91308f47c3c6e
Guy Rotshtein <100364697+GuyRotshtein@users.noreply.github.com>
2023-05-13 21:31:20 +0300

    Updates JS and addClothing page

--- js/script.js
+++ js/script.js
@@ -6,22 +6,56 @@ let clothingCategory = document.addClothingForm.category.value;
 let clothingBrand = document.addClothingForm.brand.value;
 let errorFlag = 0;
 
+    //hiding all error texts on the page in case some were fixed
+    document.getElementById(`invalidName`).style.display = `none`;
+    document.getElementById(`invalidSize`).style.display = `none`;
+    document.getElementById(`invalidBrand`).style.display = `none`;
+    document.getElementById(`invalidCategory`).style.display = `none`;
+    document.getElementById(`invalidCloset`).style.display = `none`;
+
     if (!clothingName) {
         window.setTimeout(function() { document.addClothingForm.item.focus(); },0);
         document.getElementById(`invalidName`).style.display = `block`;
-        window.alert(`Please write The clothing item's name`)
+        window.alert(`Please write The clothing item's name`);
         errorFlag = 1;
     }
     if (clothingSize == `default`){
-        window.setTimeout(function() { document.addClothingForm.item.focus(); },0);
-        document.getElementById(`invalidName`).style.display = `block`;
-        window.alert(`Please write The clothing item's name`)
-        if (errorFlag = 0){
+        window.setTimeout(function() { document.addClothingForm.size.focus(); },0);
+        document.getElementById(`invalidSize`).style.display = `block`;
+        window.alert(`Please select a size from the dropdown list`);
+        if (errorFlag == 0){
             window.setTimeout(function() { document.addClothingForm.size.focus(); },0);
             errorFlag = 1;
         }
     }
-
+    if (clothingCloset == `default`){
+        window.setTimeout(function() { document.addClothingForm.closet.focus(); },0);
+        document.getElementById(`invalidCloset`).style.display = `block`;
+        window.alert(`Please select a closet from the dropdown list`);
+        if (errorFlag == 0){
+            window.setTimeout(function() { document.addClothingForm.closet.focus(); },0);
+            errorFlag = 1;
+        }
+    }
+    if (clothingCategory == `default`){
+        window.setTimeout(function() { document.addClothingForm.category.focus(); },0);
+        document.getElementById(`invalidCategory`).style.display = `block`;
+        window.alert(`Please select a category from the dropdown list`);
+        if (errorFlag == 0){
+            window.setTimeout(function() { document.addClothingForm.category.focus(); },0);
+            errorFlag = 1;
+        }
+    }
+    if (clothingBrand == `default`){
+        window.setTimeout(function() { document.addClothingForm.brand.focus(); },0);
+        document.getElementById(`invalidBrand`).style.display = `block`;
+        window.alert(`Please select a brand from the dropdown list`);
+        if (errorFlag == 0){
+            window.setTimeout(function() { document.addClothingForm.brand.focus(); },0);
+            errorFlag = 1;
+        }
+    }
+    console.log(`final: ` +errorFlag);
     // if there is an error, don't submit the form
     if (errorFlag == 1){
         errorFlag = 0;
